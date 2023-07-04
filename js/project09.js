1cf9f75dd5343fbdb6c3285d47bd4557b23ca098
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-07-05 01:55:26 +0900

    Add files via upload

--- js/project09.js
+++ js/project09.js
@@ -1,5 +1,22 @@
 $(function () {
-    $('.header_top .top_inner i').on('click', function () {
-        $('.header_top').hide();
-    })
-})
+  $(".header_top .top_inner i").on("click", function () {
+    $(".header_top").hide();
+  });
+
+  var bottom = $(".header_bottom").offset().top;
+  $(window).scroll(function () {
+    var window = $(this).scrollTop();
+
+    if (bottom <= window) {
+      $(".header_bottom").addClass("fixed");
+    } else {
+      $(".header_bottom").removeClass("fixed");
+    }
+  });
+
+  $(".mainslide").sick({
+    arrows: false,
+    autoplay: true,
+    autoplayspeed: 5000,
+  });
+});
