5ca21eafa4b77bb18aaa2a698a9535fb71479a7e
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-07-06 18:27:44 +0900

    Add files via upload

--- js/project09.js
+++ js/project09.js
@@ -44,6 +44,71 @@ $(function () {
     const idx = $(this).index();
     $('.main_content .tab_con li').eq(idx).addClass('on').siblings().removeClass('on')
     $(this).addClass("on").siblings().removeClass("on");
+  });
+
+
+  function updateTimer() {
+    const future = Date.parse("2023/07/06 23:00:00");
+    const now = new Date();
+    const diff = future - now;
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const mins = Math.floor(diff / (1000 * 60));
+    const secs = Math.floor(diff / 1000);
+
+    const d = days;
+    const h = hours - days * 24;
+    const m = mins - hours * 60;
+    const s = secs - mins * 60;
+    document.getElementById("timer")
+      .innerHTML =
+      '<div>' + d + '<span>Days</span></div>' +
+      '<div>' + h + '<span>Hours</span></div>' +
+      '<div>' + m + '<span>Minutes</span></div>' +
+      '<div>' + s + '<span>Seconds</span></div>';
+  }
+
+  setInterval(updateTimer, 1000);
+
+  $('.main_sale .sale_slide').slick({
+    slidesToShow: 4,
+    arrows: false,
+
+  });
+
+  $(".main_sale .arrows .left").on("click", function () {
+    $(".sale_slide").slick("slickPrev");
+  });
+  $(".main_sale .arrows .right").on("click", function () {
+    $(".sale_slide").slick("slickNext");
+  });
+
+  $('.main_sale .heart').click(function () {
+    $(this).css('color', 'red');
+  });
+
+
+
+  // $('.main_best .cart').on('click', function (e) {
+  //   e.preventDefault();
+  //   $(this).toggleClass('on');
+  // })
+
+  let cartNum = 0;
+
+  $('.main_best .cart').on('click', function (e) {
+    e.preventDefault();
+
+    if ($(this).hasClass('on')) {
+      alert('이미 장바구니에 담았어요****');
+      return;
+    } else {
+      cartNum = cartNum + 1;
+    }
+    $(this).addClass('on');
+    console.log(cartNum);
+    $('.header .bottom_icon small').text(cartNum);
   })
 
 });
