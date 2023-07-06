c3ac01937189c63135055d9549a4e613b4bad7d3
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-07-07 04:37:31 +0900

    Add files via upload

--- js/project09.js
+++ js/project09.js
@@ -21,31 +21,31 @@ $(function () {
   });
 
   //롤링 배너 복제본 생성
-  let roller = document.querySelector('.best_content');
-  roller.id = 'roller1';
+  let roller = document.querySelector(".best_content");
+  roller.id = "roller1";
 
   let clone = roller.cloneNode(true);
-  clone.id = 'roller2';
-  document.querySelector('.wrap').appendChild(clone); //부착
+  clone.id = "roller2";
+  document.querySelector(".wrap").appendChild(clone); //부착
 
   //원본, 복제본 배너 위치 지정
-  document.querySelector('#roller1').style.left = '0px';
-  document.querySelector('#roller2').style.left = document.querySelector('.best_content ul').offsetWidth + 'px';
+  document.querySelector("#roller1").style.left = "0px";
+  document.querySelector("#roller2").style.left =
+    document.querySelector(".best_content ul").offsetWidth + "px";
 
   //클래스 할당
-  roller.classList.add('original');
-  clone.classList.add('clone');
+  roller.classList.add("original");
+  clone.classList.add("clone");
 
-  $('.best_content .heart').click(function () {
-    $(this).css('color', 'red');
-  });
-
-  $('.main_content .tab_list li').on('click', function () {
+  $(".main_content .tab_list li").on("click", function () {
     const idx = $(this).index();
-    $('.main_content .tab_con li').eq(idx).addClass('on').siblings().removeClass('on')
+    $(".main_content .tab_con li")
+      .eq(idx)
+      .addClass("on")
+      .siblings()
+      .removeClass("on");
     $(this).addClass("on").siblings().removeClass("on");
   });
-
 
   function updateTimer() {
     const future = Date.parse("2023/07/06 23:00:00");
@@ -61,20 +61,26 @@ $(function () {
     const h = hours - days * 24;
     const m = mins - hours * 60;
     const s = secs - mins * 60;
-    document.getElementById("timer")
-      .innerHTML =
-      '<div>' + d + '<span>Days</span></div>' +
-      '<div>' + h + '<span>Hours</span></div>' +
-      '<div>' + m + '<span>Minutes</span></div>' +
-      '<div>' + s + '<span>Seconds</span></div>';
+    document.getElementById("timer").innerHTML =
+      "<div>" +
+      d +
+      "<span>Days</span></div>" +
+      "<div>" +
+      h +
+      "<span>Hours</span></div>" +
+      "<div>" +
+      m +
+      "<span>Minutes</span></div>" +
+      "<div>" +
+      s +
+      "<span>Seconds</span></div>";
   }
 
   setInterval(updateTimer, 1000);
 
-  $('.main_sale .sale_slide').slick({
+  $(".main_sale .sale_slide").slick({
     slidesToShow: 4,
     arrows: false,
-
   });
 
   $(".main_sale .arrows .left").on("click", function () {
@@ -84,31 +90,23 @@ $(function () {
     $(".sale_slide").slick("slickNext");
   });
 
-  $('.main_sale .heart').click(function () {
-    $(this).css('color', 'red');
+  $(".heart").on("click", function (e) {
+    e.preventDefault();
+    $(this).toggleClass("on");
   });
-
-
-
-  // $('.main_best .cart').on('click', function (e) {
-  //   e.preventDefault();
-  //   $(this).toggleClass('on');
-  // })
-
   let cartNum = 0;
 
-  $('.main_best .cart').on('click', function (e) {
+  $(".cart").on("click", function (e) {
     e.preventDefault();
 
-    if ($(this).hasClass('on')) {
-      alert('이미 장바구니에 담았어요****');
+    if ($(this).hasClass("on")) {
+      alert("이미 장바구니에 담았어요");
       return;
     } else {
       cartNum = cartNum + 1;
     }
-    $(this).addClass('on');
+    $(this).addClass("on");
     console.log(cartNum);
-    $('.header .bottom_icon small').text(cartNum);
-  })
-
+    $(".header .bottom_icon small").text(cartNum);
+  });
 });
